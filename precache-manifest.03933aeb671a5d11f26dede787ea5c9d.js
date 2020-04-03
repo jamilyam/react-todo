8d8f99c1f1557b93c4d96e5e68dc67582828e48d
Jamilya <jamilyam312@gmail.com>
2020-04-03 22:48:50 +0600

    Updates

---
+++ precache-manifest.03933aeb671a5d11f26dede787ea5c9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1657361e0c4084f4bd1520ae88f68ec",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "debec866d70ef990337e",
+    "url": "/react-todo/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "7d8c0a35fa1ce81a7eb1",
+    "url": "/react-todo/static/css/main.6a05a5ee.chunk.css"
+  },
+  {
+    "revision": "debec866d70ef990337e",
+    "url": "/react-todo/static/js/2.25c0bb9e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-todo/static/js/2.25c0bb9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8c0a35fa1ce81a7eb1",
+    "url": "/react-todo/static/js/main.5caf6ff8.chunk.js"
+  },
+  {
+    "revision": "f83eb8c4877b55edc292",
+    "url": "/react-todo/static/js/runtime-main.560d53dc.js"
+  }
+]);
